63b8e597d90e7ad488d10348593d4f3a47b4aea5
Shubham Yadav <shubhayadav2503@gmail.com>
2023-09-19 00:27:19 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 
 
 function App() {
-  const b=<h1>Shubham</h1>;
+  // const b=<h1>Shubham</h1>;
   return (
     <div className="App">
-     {b};
-    </div>
+     <Display salary={3000} position="sde" company="amazon"  />
+     </div>
   );
-}
+};
+const Display=(props)=>{
+  const age=18;
+  const isGreen=true;
+  return (<div>
+      
+    {age>18? <h1>eligible</h1>: <h1>not eligible</h1> }
+    { <div style={{color:isGreen?"green":"red"}}> this is button</div> }
+    {isGreen&& <button>this is button</button> }
+
+  </div>);
+}; 
 
 export default App;
