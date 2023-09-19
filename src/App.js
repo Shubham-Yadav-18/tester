44399959f0281e375426be8b0e5a336d66482b5b
Shubham Yadav <shubhayadav2503@gmail.com>
2023-09-20 01:47:28 +0530

    crudd functionality is added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import { useState } from "react";
-
+import TaskItem from "./component/NewTask";
 function App() {
   const [list, setList] = useState([]);
-  const [task,setTask]=useState("");
+  const [newTask, setTask] = useState("");
+  const deleteTask = (taskId) => {
+    setList(list.filter((task) => task.id !== taskId));
+  };
+  const completTask = (id) => {
+    setList(
+      list.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else return task;
+      })
+    );
+  };
   return (
-    <div style={{textAlign:"center"}} >
-       <div className="addtask">
-         <input value={task} onChange={(e)=>{
+    <div style={{ textAlign: "center" }}>
+      <div className="addtask">
+        <input
+          value={newTask}
+          onChange={(e) => {
             setTask(e.target.value);
-         }}></input>
-         <button onClick={()=>{
-            setList([...list,task]);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            const taskobj = {
+              id: list.length === 0 ? 1 : list[list.length - 1].id + 1,
+              taskName: newTask,
+              completed: false,
+            };
+            setList([...list, taskobj]);
             setTask("");
-         }}>Add Task</button>
-       </div>
-       {
-        list.map((item,key)=>{
-            return <div style={{margin: "10px" }} key={key}> {item}</div>
-        })
-       }
+          }}
+        >
+          Add Task
+        </button>
+      </div>
+      {list.map((item) => {
+        return (
+          <TaskItem
+            taskName={item.taskName}
+            id={item.id}
+            completed={item.completed}
+            deleteTask={deleteTask}
+            completTask={completTask}            
+
+          />
+        );
+      })}
     </div>
   );
 }
