1fb7e5a3da9d3e17c27c8b8981dfd18e87776546
Shubham Yadav <shubhayadav2503@gmail.com>
2023-09-20 00:15:56 +0530

    add task in todo list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [list, setList] = useState([]);
+  const [task,setTask]=useState("");
   return (
-    <div className="App">
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Increase
-      </button>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        Decrease
-      </button>
-      <button
-        onClick={() => {
-          setCount(0);
-        }}
-      >
-        Set to Zero
-      </button>
-      <h1>{count}</h1>
+    <div style={{textAlign:"center"}} >
+       <div className="addtask">
+         <input value={task} onChange={(e)=>{
+            setTask(e.target.value);
+         }}></input>
+         <button onClick={()=>{
+            setList([...list,task]);
+            setTask("");
+         }}>Add Task</button>
+       </div>
+       {
+        list.map((item,key)=>{
+            return <div style={{margin: "10px" }} key={key}> {item}</div>
+        })
+       }
     </div>
   );
 }
