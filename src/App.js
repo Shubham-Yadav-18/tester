0eeaa4cdb98f36fa9b16142e04dc4adaea9dd4df
Shubham Yadav <shubhayadav2503@gmail.com>
2023-09-22 03:52:20 +0530

    use react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Navbar from "./pages/Navbar";
-import Profile from "./pages/Profile";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Form from "./pages/Form";
 
 
 const App = () => {
-   const client=new QueryClient();
+  
   return (
     <div>
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1>Page is not found</h1>} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form/>
     </div>
   );
 };
